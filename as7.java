cb7afe10f3ca4735faa4756d81654302d62bc619
Prateek Gupta <97242129+prateekscode@users.noreply.github.com>
2023-04-09 17:59:19 +0530

    Assignment taking input
    Assignment 2

---
+++ as7.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class as7{
+public static void main(String[] args){
+Scanner sc=new Scanner(System.in);
+System.out.println("Enter the two strings");
+String s1=sc.next();
+String s2=sc.next();
+System.out.println(s1+s2);
+}
+}
